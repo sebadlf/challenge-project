bcaa618e6d1a52938341aecc1e62012cde45527e
Sebastián de la Fuente <sebadlf@gmail.com>
2021-02-13 22:18:48 -0300

    Basic visual component implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Layout } from 'antd';
+
+import useAxios from 'axios-hooks';
 
 import './App.css';
 import AutoComplete from './components/autocomplete/autocomplete';
 
 import 'antd/dist/antd.css';
+import { ApiResponse } from './types';
 
 const { Header, Content, Footer } = Layout;
 
 function App() {
+	const [result, refetch] = useAxios<ApiResponse, string>('http://localhost:3030/cities');
+
+	const { data, loading, error } = result;
+
+	const cities = data?.data ?? [];
+
+	const options = cities.slice(0, 5).map((c) => ({
+		value: c.geonameid,
+		label: c.name,
+		subLabel: c.subcountry ? `${c.subcountry} - ${c.country}` : c.country,
+	}));
+
+	console.log(options);
+
+	const [value, setValue] = useState<number[]>([]);
+
 	return (
 		<div className="App">
 			<Layout className="layout">
@@ -17,7 +36,8 @@ function App() {
 				</Header>
 				<Content style={{ padding: '2rem' }}>
 					<div className="site-layout-content">
-						<AutoComplete options={[]} />
+						<AutoComplete options={options} value={value} onChange={setValue} />
+						<div>{value}</div>
 					</div>
 				</Content>
 				<Footer style={{ textAlign: 'center' }}>Code Challenge - Sebastián de la Fuente</Footer>
--- src/components/autocomplete/autocomplete.tsx
+++ src/components/autocomplete/autocomplete.tsx
@@ -1,5 +1,9 @@
-import React from 'react';
-import { AutoComplete } from 'antd';
+import React, { useState } from 'react';
+import { AutoComplete, Input, Select } from 'antd';
+import { FilterTwoTone, LoadingOutlined } from '@ant-design/icons';
+import Checkbox from 'antd/lib/checkbox/Checkbox';
+
+import './autocomplete.css';
 
 interface AutoCompleteOption {
 	value: number;
@@ -9,10 +13,55 @@ interface AutoCompleteOption {
 
 interface AutoCompleteProps {
 	options: AutoCompleteOption[];
+	value: number[];
+	onChange: (value: number[]) => void;
 }
 
-const renderOptions = (options: AutoCompleteOption[]) => options.map((op) => <div>{op.label}</div>);
+const AutocompleteView = ({ options, value, onChange }: AutoCompleteProps) => {
+	const [localValue, setLocalValue] = useState('');
 
-const AutocompleteView = ({ options }: AutoCompleteProps) => <AutoComplete options={options} />;
+	const renderOptions = (localOptions: AutoCompleteOption[]) =>
+		localOptions.map((op) => ({
+			value: op.value,
+			label: (
+				<div key={op.value} className="AutoComplete-Item">
+					<div className="AutoComplete-CheckboxContainer">
+						<Checkbox checked={value.includes(op.value)} />
+					</div>
+					<div className="AutoComplete-LabelContainer">
+						<div className="AutoComplete-Label">{op.label}</div>
+						<div className="AutoComplete-SubLabel">{op.subLabel}</div>
+					</div>
+				</div>
+			),
+		}));
+
+	return (
+		<AutoComplete
+			value={localValue}
+			onChange={setLocalValue}
+			dropdownMatchSelectWidth={252}
+			style={{ minWidth: '20rem' }}
+			onSelect={(selectedValue) => {
+				const selectedValueNum = Number(selectedValue);
+
+				const newValue = value.includes(Number(selectedValue))
+					? value.filter((v) => v !== selectedValueNum)
+					: [...value, selectedValueNum];
+
+				setLocalValue('');
+
+				onChange(newValue);
+			}}
+			options={renderOptions(options)}
+		>
+			<Input
+				prefix={<FilterTwoTone twoToneColor="#52c41a" />}
+				suffix={<LoadingOutlined />}
+				placeholder="input here"
+			/>
+		</AutoComplete>
+	);
+};
 
 export default AutocompleteView;
